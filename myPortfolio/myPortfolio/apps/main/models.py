3b14fcd897cadcbf2b08d22f05b8d73e1d5edbc4
Терещенко Дмитрий <dima123-99@mail.ru>
2019-09-03 21:11:55 +0300

    Added first model (Comment)

--- myPortfolio/myPortfolio/apps/main/models.py
+++ myPortfolio/myPortfolio/apps/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+#models.ForeignKey(name_class, on_delete = models.CASCADE)
+class Comment(models.Model):
+    author_name = models.CharField('имя автора', max_length = 50)
+    comment_text = models.CharField('текст комментария', max_length = 200)
+    date = models.DateTimeField('дата комментария')
+
